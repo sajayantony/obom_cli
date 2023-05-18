37e8ccf75d12c1f70dba65ca059bbc4b636ae9d8
Brett Bishop <107079073+bbishopMSFT@users.noreply.github.com>
2023-05-18 14:24:46 -0500

    Added creator to show command

--- internal/sbom.go
+++ internal/sbom.go
@@ -48,6 +48,20 @@ func LoadSBOM(filename string) (*v2_3.Document, *oci.Descriptor, error) {
 	return doc, desc, nil
 }
 
+func PrintCreatorInfo(doc *v2_3.Document) {
+	size := len(doc.CreationInfo.Creators)
+	if size == 1 {
+		creator := doc.CreationInfo.Creators[0]
+		fmt.Printf("Creator:               %s\n", creator.Creator)
+	} else if size > 1 {
+		firstItem := doc.CreationInfo.Creators[0]
+		fmt.Printf("Creators:              %s\n", firstItem.Creator)
+		for _, creator := range doc.CreationInfo.Creators[1:] {
+			fmt.Printf("                       %s\n", creator.Creator)
+		}
+	}
+}
+
 // PrintSBOMSummary returns the SPDX summary from the SBOM
 func PrintSBOMSummary(doc *v2_3.Document, desc *oci.Descriptor) {
 	fmt.Println(strings.Repeat("=", 80))
@@ -56,6 +70,7 @@ func PrintSBOMSummary(doc *v2_3.Document, desc *oci.Descriptor) {
 	fmt.Printf("Document Namespace:    %s\n", doc.DocumentNamespace)
 	fmt.Printf("SPDX Version:          %s\n", doc.SPDXVersion)
 	fmt.Printf("Creation Date:         %s\n", doc.CreationInfo.Created)
+	PrintCreatorInfo(doc)
 	fmt.Printf("Packages:              %d\n", len(doc.Packages))
 	fmt.Printf("Files:                 %d\n", len(doc.Files))
 	fmt.Printf("Digest:                %s\n", desc.Digest)
